ffacd982a3bfe88d1f4bcbed34d52a59e35c5d5f
Takata <felipe.alvarenga@edu.unipar.br>
2022-06-19 01:10:03 -0300

    Feat Detalhe Integrantes

---
+++ app/src/main/java/com/felipe/splashscreen/Detalhes_Integrantes.java
@@ -0,0 +1,16 @@
+package com.felipe.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Detalhes_Integrantes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes_integrantes);
+
+    }
+
+}
