6feb4de53817b5931ac8ff6cd2fa90fce431e822
Takata <felipe.alvarenga@edu.unipar.br>
2022-06-19 01:24:51 -0300

    feat Detalhes Integrantes

--- app/src/main/java/com/felipe/splashscreen/Detalhes_Integrantes.java
+++ app/src/main/java/com/felipe/splashscreen/Detalhes_Integrantes.java
@@ -2,6 +2,7 @@ package com.felipe.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Detalhes_Integrantes extends AppCompatActivity {
