d228c56e3efc0ffc22b691bd3fff39a580de1093
Takata <felipe.alvarenga@edu.unipar.br>
2022-06-18 23:33:59 -0300

    Delete First and Second tabbed

--- app/src/main/java/com/felipe/splashscreen/FirstTabbedViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.felipe.splashscreen;
-
-import androidx.lifecycle.ViewModel;
-
-public class FirstTabbedViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/felipe/splashscreen/SecondTabbedViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.felipe.splashscreen;
-
-import androidx.lifecycle.ViewModel;
-
-public class SecondTabbedViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
